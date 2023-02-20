4d1a2c035795ab55550f0beeb1350b41a45a01e4
tciryanw <tciwrccwest@gmail.com>
2023-02-20 22:11:14 +0100

    paytable added, updated rules little bit

--- resources/js/game.js
+++ resources/js/game.js
@@ -3,6 +3,9 @@ var collect_button = document.getElementById('collect_button')
 var gamble_button = document.getElementById('gamble_button')
 var reset_game = false
 
+var options_after = document.getElementById('options_after')
+var options_after_reason = document.getElementById('options_after_reason')
+
 var dice_number = document.getElementById('dice_number')
 var inplay_number = document.getElementById('inplay_number')
 var credit_number = document.getElementById('credit_number')
@@ -11,18 +14,27 @@ var snake_final_pos = [9,12,4,31,0]
 var ladder_array = [1,3,18,24,40]
 var ladder_final_pos = [11,17,28,45,42]
 
-var win_3x_pos = [2,8,13,19,27,34,38,45]
+var win_3x_pos = [2,8,13,19,27,34,38,46]
 var win_3x_payout = [2,4,6,8,10,14,20,30]
 
-var player1_coin = document.createElement('div')
-player1_coin.setAttribute('id','player_coin1')
-player1_coin.innerText = 'PLAYER'
+var win_single_pos = [5,10,16,21,25,32,36,45]
+var win_single_symbols = [0,1,2,3,4,5,6,7]
+
+// symbols followed by payout once have collected 3x
+// cherry(2x) -> lemon(4x) -> orange(6x) -> mango(8x) -> grapes (10x) -> bell (14x) -> melon (20x) -> bar (30x)
+var paytable_state = [0,0,0,0,0,0,0,0]
+var paytable_payout = [2,4,6,8,10,14,20,30]
 
 
 var inplay_credit = 0
 var bank_credit = 100
 var current_player = true
 var player_counter = [0,0,0]
+
+var player1_coin = document.createElement('div')
+player1_coin.setAttribute('id','player_coin1')
+player1_coin.innerText = 'Player'
+
 
 window.addEventListener('load',start)
 window.addEventListener('load',collect)
@@ -66,6 +78,7 @@ function start_function() {
 function dice_rolled(){
     lights_off()
     var dicenumber_rolled = random()
+    //var dicenumber_rolled = 5
     dice_number.innerText = dicenumber_rolled
     append_element(player_picker(), dicenumber_rolled)
 }
@@ -129,6 +142,12 @@ function box_generate(i){
     return light_active_box;
 }
 
+function box_generate(i){
+    var light_active_box = document.createElement('div')
+    light_active_box.setAttribute('id','light_active_paytable')
+    return light_active_box;
+}
+
 function lights_off(){
     var lineItem = document.querySelectorAll('.lightable')
     lineItem.forEach((item, index) => { // Add item to closure
@@ -150,19 +169,50 @@ function lights_on(number){
 
 function append_element(player, dicenumber_rolled){
     if(reset_game === true) {
+        var player_next_position = document.getElementById(id_creator(0))
+        player_next_position.append(player1_coin)
         player_counter[player] = 0
+        paytable_state = [0,0,0,0,0,0,0,0]
+        for(i=0; i<win_single_symbols.length; i++){
+            var light_paytable_id = '.lightable_paytable_' + win_single_symbols[i] 
+            var lineItem = document.querySelectorAll(light_paytable_id)
+            lineItem.forEach((item, index) => { // Add item to closure
+                    item.innerHTML = ""
+            });
+        }
         reset_game = false
     }
-    counter(player)
-    console.log(player_counter[player])
-    update_log(player_counter[player], 'roll_regular')
-    update_inplay(0)
-    var player_next_position = document.getElementById(id_creator(player_counter[player]))
-    player_next_position.append(player1_coin)
-    snake_or_ladder(player_counter[player],player)
-    payout_check(player_counter[player],player)
-    lights_on(player_counter[player])
+    setTimeout(() => { // Removed i from closure
+        counter(player)
+        console.log(player_counter[player])
+        update_log(player_counter[player], 'roll_regular')
+        update_inplay(0)
+        var player_next_position = document.getElementById(id_creator(player_counter[player]))
+        player_next_position.append(player1_coin)
+        reset_game = true
+        snake_or_ladder(player_counter[player],player)
+        payout_check(player_counter[player],player)
+        lights_on(player_counter[player])
+        options_after_game()
+    }, 500) // Increase the delay time by 20ms for each item
 
+}
+
+function options_after_game() {
+    if(reset_game === true) {
+        options_after.innerText = "start (from 0)"
+        options_after_reason.innerText = "(hit empty tile, lost any inplay credit)"
+    }
+    if(reset_game === false) {
+        if(inplay_credit > 0) {
+        options_after.innerText = "continue or collect "+inplay_credit
+        options_after_reason.innerText = "(you risk losing all inplay credit)"
+
+        } else {
+            options_after.innerText = "continue"
+            options_after_reason.innerText = "(you have hit a single fruit, ladder or snake)"
+        }
+    }
 }
 
 function snake_or_ladder(counter , player){
@@ -180,15 +230,45 @@ function snake_or_ladder(counter , player){
             update_log(ladder_final_pos[j].toString(), 'roll_ladder-'+ladder_array[j]+'to'+player_counter[player])
         }
     }
-    
 }
 
-function payout_check(counter , player){
+function update_paytable_single(symbol_id) {
+    update_log(player_counter[1], paytable_payout[symbol_id])
+    if(paytable_state[symbol_id] > 1) {
+        update_inplay(paytable_payout[symbol_id] + inplay_credit)
+        paytable_state[symbol_id] = 0
+    } else {
+        paytable_state[symbol_id] = (paytable_state[symbol_id] + 1)
+        update_log(player_counter[1], "Paytable state updated to: "+paytable_state[symbol_id])
+    }
+    paytable_lights_update(symbol_id, paytable_state[symbol_id])
+}
+
+function paytable_lights_update(symbol_id,state){
+    var class_paytable = '.lightable_paytable_' + symbol_id
+    var lineItem = document.querySelectorAll(class_paytable)
+    lineItem.forEach((item, index) => { // Add item to closure
+    setTimeout(() => { // Removed i from closure
+        var box_id = (item.id).replace('paytable_lightable_', '');
+        if(box_id < (state+1)) {
+            item.append(box_generate(box_id))
+            item.innerText = "LIGHT"
+        }
+        }, 5 * (index + 2)) // Increase the delay time by 20ms for each item
+    });
+}
+
+function payout_check(counter,player){
     for(d=0; d<win_3x_pos.length; d++){
         if(counter == win_3x_pos[d]){
             update_inplay(win_3x_payout[d] + inplay_credit)
-        } else {
-            reset_game = true
+            reset_game = false
+        }
+    }
+    for(e=0; e<win_single_pos.length; e++){
+        if(counter == win_single_pos[e]){
+            update_paytable_single(win_single_symbols[e])
+            reset_game = false
         }
     }
 }
@@ -196,6 +276,7 @@ function payout_check(counter , player){
 function after_snake_or_ladder(player){
     var player_next_position = document.getElementById(id_creator(player_counter[player]))
         player_next_position.append(player1_coin)
+        reset_game = false
 }
 
 
